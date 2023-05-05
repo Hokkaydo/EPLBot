c067011dda259e5ff52859d6ace59f20890864ce
hokkaydo <hokkaydo@users.noreply.github.com>
2023-05-05 18:19:15 +0200

    Remove unused imports

--- src/main/java/com/github/hokkaydo/eplbot/Config.java
+++ src/main/java/com/github/hokkaydo/eplbot/Config.java
@@ -1,14 +1,11 @@
 package com.github.hokkaydo.eplbot;
 
-import kotlin.jvm.functions.Function22;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Function;
 import java.util.stream.Collectors;
