84b88fb71fee11b3e10192c50f1cab5a6abd7f14
hokkaydo <hokkaydo@users.noreply.github.com>
2023-05-05 17:03:10 +0200

    Add message externalization

---
+++ src/main/java/com/github/hokkaydo/eplbot/Strings.java
@@ -0,0 +1,33 @@
+package com.github.hokkaydo.eplbot;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Strings {
+
+    private Strings() {}
+    private static final Map<String, String> STRINGS_MAP = new HashMap<>();
+
+    public static void load() throws JSONException {
+        InputStream stream = Strings.class.getClassLoader().getResourceAsStream("strings.json");
+        assert stream != null;
+        JSONObject object = new JSONObject(new JSONTokener(stream));
+        JSONArray names = object.names();
+        for (int i = 0; i < names.length(); i++) {
+            String key = names.getString(i);
+            STRINGS_MAP.put(key, object.getString(key));
+        }
+    }
+    private static final String STRING_NOT_FOUND = "Erreur de traduction. Veuillez la signaler à la modération";
+
+    public static String getString(String key) {
+        return STRINGS_MAP.getOrDefault(key, STRING_NOT_FOUND);
+    }
+
+}
