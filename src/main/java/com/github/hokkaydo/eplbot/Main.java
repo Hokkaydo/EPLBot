535f200f3193faf18b8eecf7eb7859fa09eaacea
hokkaydo <hokkaydo@users.noreply.github.com>
2023-05-05 16:59:59 +0200

    Add Dotenv lib to read .env file

--- src/main/java/com/github/hokkaydo/eplbot/Main.java
+++ src/main/java/com/github/hokkaydo/eplbot/Main.java
@@ -1,5 +1,7 @@
 package com.github.hokkaydo.eplbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.internal.DotenvReader;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -16,14 +18,14 @@ public class Main {
     private static JDA jda;
 
     public static void main(String[] args) throws InterruptedException {
-            final String token = System.getenv("DISCORD_BOT_TOKEN");
-            jda = JDABuilder.createDefault(token)
-                          .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-                          .disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
-                          .setBulkDeleteSplittingEnabled(false)
-                          .setActivity(Activity.watching("you"))
-                          .build();
-            jda.awaitReady();
+        final String token = Dotenv.load().get("DISCORD_BOT_TOKEN");
+        jda = JDABuilder.createDefault(token)
+                      .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                      .disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
+                      .setBulkDeleteSplittingEnabled(false)
+                      .setActivity(Activity.watching("you"))
+                      .build();
+        jda.awaitReady();
     }
 
     public static JDA getJDA() {
