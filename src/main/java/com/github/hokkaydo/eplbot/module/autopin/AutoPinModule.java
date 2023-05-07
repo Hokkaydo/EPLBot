c0953351a8d62d2b2580b3c3736e1f4a92ede48f
hokkaydo <hokkaydo@users.noreply.github.com>
2023-05-07 23:27:36 +0200

    Reflect minor changes on AutoPinModule

--- src/main/java/com/github/hokkaydo/eplbot/module/autopin/AutoPinModule.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/autopin/AutoPinModule.java
@@ -1,9 +1,14 @@
 package com.github.hokkaydo.eplbot.module.autopin;
 
 import com.github.hokkaydo.eplbot.Main;
+import com.github.hokkaydo.eplbot.command.Command;
 import com.github.hokkaydo.eplbot.module.GuildModule;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
 
 public class AutoPinModule extends GuildModule {
 
@@ -14,21 +19,20 @@ public class AutoPinModule extends GuildModule {
         listener = new AutoPinListener(guild.getIdLong());
     }
 
-    @Override
-    public void enable() {
-        super.enable();
-        Main.getJDA().addEventListener(listener);
-    }
-
-    @Override
-    public void disable() {
-        super.disable();
-        Main.getJDA().removeEventListener(listener);
-    }
 
     @Override
     public String getName() {
         return "autopin";
+    }
+
+    @Override
+    public List<Command> getCommands() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ListenerAdapter> getListeners() {
+        return Collections.singletonList(listener);
     }
 
 }
