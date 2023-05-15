e0eb11c24514c235183ea66f76e5e0df575a985c
hokkaydo <hokkaydo@users.noreply.github.com>
2023-05-15 18:31:31 +0200

    Add move messages command

--- src/main/java/com/github/hokkaydo/eplbot/module/basic/BasicCommandModule.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/basic/BasicCommandModule.java
@@ -5,7 +5,6 @@ import com.github.hokkaydo.eplbot.module.Module;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,12 +14,14 @@ public class BasicCommandModule extends Module {
     private final ClearFromCommand clearFromCommand;
     private final ClearLastCommand clearLastCommand;
     private final HelpCommand helpCommand;
+    private final MoveMessagesCommand moveMessagesCommand;
     public BasicCommandModule(@NotNull Long guildId) {
         super(guildId);
         clearFromCommand = new ClearFromCommand();
         clearBetweenCommand = new ClearBetween();
         clearLastCommand = new ClearLastCommand();
         helpCommand = new HelpCommand(guildId);
+        moveMessagesCommand = new MoveMessagesCommand();
     }
 
     @Override
@@ -30,7 +31,7 @@ public class BasicCommandModule extends Module {
 
     @Override
     public List<Command> getCommands() {
-        return List.of(clearBetweenCommand, clearFromCommand, clearLastCommand, helpCommand);
+        return List.of(clearBetweenCommand, clearFromCommand, clearLastCommand, helpCommand, moveMessagesCommand);
     }
 
     @Override
