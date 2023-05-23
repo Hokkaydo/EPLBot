7a020c4da963948742f6d85ecbc60c0b25fe2026
hokkaydo <hokkaydo@users.noreply.github.com>
2023-05-23 21:18:14 +0200

    Fix pin mirroring

--- src/main/java/com/github/hokkaydo/eplbot/module/mirror/MirroredMessage.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/mirror/MirroredMessage.java
@@ -3,29 +3,24 @@ package com.github.hokkaydo.eplbot.module.mirror;
 import com.github.hokkaydo.eplbot.Main;
 import com.github.hokkaydo.eplbot.MessageUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.EmbedType;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 import net.dv8tion.jda.api.utils.FileUpload;
 
-import java.net.URL;
 import java.time.OffsetDateTime;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.regex.Pattern;
 
 public class MirroredMessage {
 
-    private static final Pattern URL_PATTERN = Pattern.compile("^https?://(?:www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b[-a-zA-Z0-9()@:%_+.~#?&/=]*$");
-
     private Message message;
     private final MessageChannel channel;
-    private boolean embed;
     private OffsetDateTime lastUpdated;
     private boolean threadOwner;
-
     public MirroredMessage(Message initialMessage, MessageChannel textChannel, boolean mirror) {
         this.channel = textChannel;
         this.lastUpdated = initialMessage.getTimeCreated();
@@ -34,26 +29,15 @@ public class MirroredMessage {
         } else {
             this.message = initialMessage;
         }
-        this.embed = mirror;
     }
 
     public void mirrorMessage(Message initialMessage) {
-
         MessageCreateAction createAction;
         String content = getContent(initialMessage);
-        if(Arrays.stream(content.split(" ")).anyMatch(s -> URL_PATTERN.matcher(s).find())) {
-            if(initialMessage.getEmbeds().isEmpty()) {
-                createAction = channel.sendMessage(initialMessage.getContentRaw());
-            } else {
-                createAction = channel.sendMessage(content);
-            }
-        }
-        else {
-            if(initialMessage.getEmbeds().isEmpty()) {
-                createAction = channel.sendMessageEmbeds(MessageUtil.toEmbed(initialMessage).build());
-            } else {
-                createAction = channel.sendMessageEmbeds(initialMessage.getEmbeds().get(0));
-            }
+        if(content.isBlank()) return;
+        createAction = channel.sendMessageEmbeds(MessageUtil.toEmbed(initialMessage).setDescription(content).build());
+        if(!initialMessage.getEmbeds().isEmpty()) {
+            createAction.addEmbeds(initialMessage.getEmbeds());
         }
         AtomicReference<MessageCreateAction> action = new AtomicReference<>(createAction);
         initialMessage.getAttachments().stream()
@@ -85,29 +69,17 @@ public class MirroredMessage {
     }
 
     public void update(Message initialMessage) {
+        updatePin(initialMessage.isPinned());
         if(!(initialMessage.getTimeEdited() == null ? initialMessage.getTimeCreated() : initialMessage.getTimeEdited()).isAfter(lastUpdated)) return;
         String content = getContent(initialMessage);
         List<Message.Attachment> attachments = initialMessage.getAttachments();
-        boolean embed = Arrays.stream(content.split(" ")).noneMatch(s -> URL_PATTERN.matcher(s).find());
         if(message.getAuthor().getIdLong() == Main.getJDA().getSelfUser().getIdLong()) {
-            if (this.embed) {
-                if (embed) {
-                    MessageEmbed messageEmbed = message.getEmbeds().get(0);
-                    if (messageEmbed == null) return;
-                    EmbedBuilder newEmbed = new EmbedBuilder(messageEmbed).setDescription(content);
-                    message.editMessageEmbeds(newEmbed.build()).setAttachments(attachments).queue();
-                } else {
-                    message.suppressEmbeds(true).map(v -> message.editMessage(content).setAttachments(attachments)).queue();
-                    this.embed = false;
-                }
-            } else {
-                if (embed) {
-                    message.editMessageEmbeds(initialMessage.getEmbeds().isEmpty() ? MessageUtil.toEmbed(initialMessage).build() : initialMessage.getEmbeds().get(0)).setContent("").setAttachments(attachments).queue();
-                    this.embed = true;
-                } else {
-                    message.editMessage(content).setAttachments(attachments).queue();
-                }
-            }
+            Optional<MessageEmbed> oldEmbed = message.getEmbeds().stream().filter(e -> e.getType() == EmbedType.RICH).findFirst();
+            if(oldEmbed.isEmpty()) return;
+            MessageEmbed newEmbed = new EmbedBuilder(oldEmbed.get()).setDescription(content).build();
+            List<MessageEmbed> current = new java.util.ArrayList<>(initialMessage.getEmbeds().stream().filter(e -> e.getType() != EmbedType.RICH).toList());
+            current.add(newEmbed);
+            message.editMessageEmbeds(current).map(m -> m.editMessageAttachments(attachments)).queue();
         }
         updatePin(initialMessage.isPinned());
         this.lastUpdated = message.getTimeEdited() == null ? message.getTimeCreated() : message.getTimeEdited();
