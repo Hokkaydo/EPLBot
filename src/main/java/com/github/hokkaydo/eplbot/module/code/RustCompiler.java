eae65e728f3c5a4eb0b3b242b11fded4fa036069
hokkaydo <hokkaydo@users.noreply.github.com>
2023-10-28 20:35:32 +0200

    Replace timeout thread by scheduled future & simplify delete temp files method

--- src/main/java/com/github/hokkaydo/eplbot/module/code/RustCompiler.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/code/RustCompiler.java
@@ -2,10 +2,21 @@ package com.github.hokkaydo.eplbot.module.code;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class RustCompiler implements Runner{
     private static final String CURRENT_DIR = System.getProperty("user.dir") + "\\src\\temp\\";
+    private static final String[] VALID_EXTENSIONS = {".exe", ".pdb", ".rs"};
+
+    private static final ScheduledExecutorService SCHEDULER = new ScheduledThreadPoolExecutor(1);
     @Override
     public String run(String input, Integer runTimeout) {
         if (containsUnsafeKeywords(input)){
@@ -33,27 +44,21 @@ public class RustCompiler implements Runner{
             if (compileExitCode == 0) {
                 String executableFileName = sourceFile.getName().replace(".rs", "");
                 File executableFile = new File(CURRENT_DIR, executableFileName);
-                executableFile.setExecutable(true);
+
+                if (executableFile.setExecutable(true)) {
+                    return "Couldn't make file executable";
+                }
                 Process run = new ProcessBuilder(new File(CURRENT_DIR, executableFileName).getAbsolutePath()).directory(new File(CURRENT_DIR)).redirectErrorStream(true).start();
-                Thread timeoutThread = new Thread(() -> {
-                    try {
-                        Thread.sleep(1000L*runTimeout);
-                        run.destroy();
-                    } catch (InterruptedException ignored) {}
-                });
-                timeoutThread.start();
+
+                ScheduledFuture<?> timeOut = SCHEDULER.schedule(() -> {}, runTimeout, TimeUnit.SECONDS);
+
                 outputStream.reset();
                 inputStream = run.getInputStream();
                 buffer = new byte[1024];
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     outputStream.write(buffer, 0, bytesRead);
                 }
-                if (run.waitFor() == 0) {
-                    timeoutThread.interrupt();
-                    deleteFiles();
-                    return outputStream.toString();
-                } else {
-                    timeoutThread.interrupt();
+                if (timeOut.isDone()) {
                     deleteFiles();
                     String output = outputStream.toString().trim();
                     if (output.isEmpty()) {
@@ -61,12 +66,15 @@ public class RustCompiler implements Runner{
                     } else {
                         return "Run failed:\n" + output;
                     }
+                } else {
+                    deleteFiles();
+                    return outputStream.toString();
                 }
             } else {
                 deleteFiles();
                 return "Compilation failed:\n" + outputStream;
             }
-        } catch (Exception e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return e.toString();
         }
@@ -80,27 +88,19 @@ public class RustCompiler implements Runner{
         }
         return false;
     }
-    private static void deleteFiles(){
-        File outputDirectory = new File(CURRENT_DIR);
-        File[] files = outputDirectory.listFiles();
-        if (files == null){
-            System.out.println("NPE trying to delete created files");
-        } else {
-            for (File file : new File(CURRENT_DIR).listFiles()) {
-                if (file.isFile() && file.getName().startsWith("temp")) {
-                    String[] validExtensions = {".exe", ".pdb", ".rs"};
-                    boolean shouldDelete = false;
-                    for (String extension : validExtensions) {
-                        if (file.getName().equals("temp"+extension)) {
-                            shouldDelete = true;
-                            break;
-                        }
-                    }
-                    if (shouldDelete) {
-                        file.delete();
-                    }
-                }
-            }
+
+    private static boolean isValidExtension(String ext) {
+        for (String validExtension : VALID_EXTENSIONS) {
+            if(ext.equals(validExtension)) return true;
         }
+        return false;
+    }
+
+    private static void deleteFiles(){
+        Optional.ofNullable(new File(CURRENT_DIR).listFiles())
+                .stream()
+                .flatMap(Arrays::stream)
+                .filter(f -> f.isFile() && isValidExtension(f.getName().replace("temp", "")))
+                .forEach(File::delete);
     }
 }
