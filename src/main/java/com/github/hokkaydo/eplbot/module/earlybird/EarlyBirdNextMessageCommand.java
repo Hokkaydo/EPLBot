b045f26913feb919ca145a1876652041a1919c26
Hokkaydo <42278077+Hokkaydo@users.noreply.github.com>
2023-12-22 18:44:11 +0100

    Feature/early bird (#43)
    * Implement EarlyBird module
    
    * Simplify configuration default values
    
    ---------
    
    Co-authored-by: hokkaydo <“hokkaydo@users.noreply.github.com”>

---
+++ src/main/java/com/github/hokkaydo/eplbot/module/earlybird/EarlyBirdNextMessageCommand.java
@@ -0,0 +1,76 @@
+package com.github.hokkaydo.eplbot.module.earlybird;
+
+import com.github.hokkaydo.eplbot.MessageUtil;
+import com.github.hokkaydo.eplbot.Strings;
+import com.github.hokkaydo.eplbot.command.Command;
+import com.github.hokkaydo.eplbot.command.CommandContext;
+import com.github.hokkaydo.eplbot.configuration.Config;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class EarlyBirdNextMessageCommand implements Command {
+
+    private final long guildId;
+    private final EarlyBirdListener earlyBirdListener;
+    EarlyBirdNextMessageCommand(long guildId, EarlyBirdListener earlyBirdListener) {
+        this.guildId = guildId;
+        this.earlyBirdListener = earlyBirdListener;
+    }
+    @Override
+    public void executeCommand(CommandContext context) {
+        Optional<String> messageOpt = context.options().stream().filter(o -> o.getName().equals("message")).findFirst().map(OptionMapping::getAsString);
+        if(messageOpt.isEmpty()) return;
+        long earlyBirdRoleId = Config.<Long>getGuildVariable(guildId, "EARLY_BIRD_ROLE_ID");
+        if(context.author().getRoles().stream().filter(r -> r.getIdLong() == earlyBirdRoleId).findFirst().isEmpty()) {
+            context.replyCallbackAction().setContent(Strings.getString("EARLY_BIRD_NOT_EARLY_BIRD")).queue();
+            return;
+        }
+        earlyBirdListener.setNextMessage(messageOpt.get());
+        MessageUtil.sendAdminMessage("Prochain message matinal enregistré par %s :%n >>> %s".formatted(context.author().getAsMention(), messageOpt.get()), guildId);
+        context.replyCallbackAction().setContent(Strings.getString("EARLY_BIRD_NEXT_MESSAGE_REGISTERED")).queue();
+    }
+
+    @Override
+    public String getName() {
+        return "earlybirdmessage";
+    }
+
+    @Override
+    public Supplier<String> getDescription() {
+        return () -> Strings.getString("EARLY_BIRD_NEXT_MESSAGE_COMMAND_DESCRIPTION");
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return Collections.singletonList(new OptionData(OptionType.STRING, "message", "Prochain message matinal que le bot enverra"));
+    }
+
+    @Override
+    public boolean ephemeralReply() {
+        return true;
+    }
+
+    @Override
+    public boolean validateChannel(MessageChannel channel) {
+        return channel instanceof GuildMessageChannel;
+    }
+
+    @Override
+    public boolean adminOnly() {
+        return false;
+    }
+
+    @Override
+    public Supplier<String> help() {
+        return () -> Strings.getString("EARLY_BIRD_NEXT_MESSAGE_COMMAND_HELP");
+    }
+
+}
