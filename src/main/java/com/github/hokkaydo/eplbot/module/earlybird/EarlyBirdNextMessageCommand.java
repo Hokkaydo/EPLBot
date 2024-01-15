1a15da04b99de6f84ce8e4f4b0c39eaccd00d505
hokkaydo <hokkaydo@users.noreply.github.com>
2024-01-15 20:35:16 +0100

    Use modal instead of command argument & add warning if message over 2k chars

--- src/main/java/com/github/hokkaydo/eplbot/module/earlybird/EarlyBirdNextMessageCommand.java
+++ src/main/java/com/github/hokkaydo/eplbot/module/earlybird/EarlyBirdNextMessageCommand.java
@@ -5,23 +5,25 @@ import com.github.hokkaydo.eplbot.Strings;
 import com.github.hokkaydo.eplbot.command.Command;
 import com.github.hokkaydo.eplbot.command.CommandContext;
 import com.github.hokkaydo.eplbot.configuration.Config;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+import net.dv8tion.jda.api.interactions.modals.ModalMapping;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Supplier;
 
-public class EarlyBirdNextMessageCommand implements Command {
+public class EarlyBirdNextMessageCommand extends ListenerAdapter implements Command {
 
     @Override
     public void executeCommand(CommandContext context) {
-        Optional<String> messageOpt = context.options().stream().filter(o -> o.getName().equals("message")).findFirst().map(OptionMapping::getAsString);
-        if(messageOpt.isEmpty()) return;
         if(!context.interaction().isGuildCommand() || context.interaction().getGuild() == null) return;
         long guildId = context.interaction().getGuild().getIdLong();
         String earlyBirdRoleId = Config.getGuildVariable(guildId, "EARLY_BIRD_ROLE_ID");
@@ -29,9 +31,15 @@ public class EarlyBirdNextMessageCommand implements Command {
             context.replyCallbackAction().setContent(Strings.getString("EARLY_BIRD_NOT_EARLY_BIRD")).queue();
             return;
         }
-        Config.updateValue(guildId, "EARLY_BIRD_NEXT_MESSAGE", messageOpt.get());
-        MessageUtil.sendAdminMessage("Prochain message matinal enregistré par %s :%n >>> %s".formatted(context.author().getAsMention(), messageOpt.get()), guildId);
-        context.replyCallbackAction().setContent(Strings.getString("EARLY_BIRD_NEXT_MESSAGE_REGISTERED")).queue();
+        Modal modal = Modal.create("earlybird-message", "Message matinal")
+                              .addActionRow(TextInput.create("message", "Message", TextInputStyle.PARAGRAPH).build())
+                              .build();
+        context.interaction().replyModal(modal).queue();
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return Collections.emptyList();
     }
 
     @Override
@@ -45,8 +53,20 @@ public class EarlyBirdNextMessageCommand implements Command {
     }
 
     @Override
-    public List<OptionData> getOptions() {
-        return Collections.singletonList(new OptionData(OptionType.STRING, "message", "Prochain message matinal que le bot enverra", true));
+    public void onModalInteraction (ModalInteractionEvent event) {
+        if(event.getGuild() == null) return;
+        String id = event.getModalId();
+        if(!id.equals("earlybird-message")) return;
+        ModalMapping contentMap = event.getInteraction().getValue("message");
+        if(contentMap == null) return;
+        String content = contentMap.getAsString();
+        if(content.length() > Message.MAX_CONTENT_LENGTH) {
+            event.reply(Strings.getString("EARLY_BIRD_MESSAGE_TOO_LONG").formatted(Message.MAX_CONTENT_LENGTH)).queue();
+            return;
+        }
+        Config.updateValue(event.getGuild().getIdLong(), "EARLY_BIRD_NEXT_MESSAGE", content);
+        MessageUtil.sendAdminMessage("Prochain message matinal enregistré par %s :%n >>> %s".formatted(event.getUser().getAsMention(), content), event.getGuild().getIdLong());
+        event.reply(Strings.getString("EARLY_BIRD_NEXT_MESSAGE_REGISTERED")).queue();
     }
 
     @Override
