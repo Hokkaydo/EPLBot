93084ebc76a9c2c276a33a9030059ff812101eae
hokkaydo <hokkaydo@users.noreply.github.com>
2025-01-20 22:04:32 +0100

    Change memory consumption log format

--- src/main/java/com/github/hokkaydo/eplbot/Main.java
+++ src/main/java/com/github/hokkaydo/eplbot/Main.java
@@ -220,7 +220,7 @@ public class Main {
         long totalMemory = Runtime.getRuntime().totalMemory();
         long freeMemory = Runtime.getRuntime().freeMemory();
         long usedMemory = totalMemory - freeMemory;
-        return "Memory usage : %d / %d (%d%%)".formatted(usedMemory, totalMemory, (usedMemory * 100) / totalMemory);
+        return "Memory usage : %.4f / %.4f (%d%%)".formatted(usedMemory/1_000_000_000, totalMemory/1_000_000_000, (usedMemory * 100) / totalMemory);
     }
 
     private static <T> T instantiate(Class<T> clazz, Long guildId) {
