8828ea4c3b4879a4f03e5c6d046b0d240f42b65e
emecdelam <kiwi.bp.surviv@gmail.com>
2026-01-19 16:05:34 +0100

    separation for guilds

--- src/main/java/com/github/hokkaydo/eplbot/database/DatabaseManager.java
+++ src/main/java/com/github/hokkaydo/eplbot/database/DatabaseManager.java
@@ -31,7 +31,7 @@ public class DatabaseManager {
             new TableModel("notices", Map.of("author_id", TEXT, "subject_id", TEXT, "content", TEXT, "timestamp", "timestamp", "type", TEXT)),
             new TableModel("bookmarks", Map.of("user_id", INTEGER, "message_id", INTEGER, "description", TEXT, "message_link", TEXT)),
             new TableModel("course_helpers", Map.of("channel_id", INTEGER, "user_id", INTEGER, "allows_ping", INTEGER, "is_tutor", INTEGER)),
-            new TableModel("events", Map.of("timestamp", INTEGER, "event_type", TEXT, "user_id", INTEGER, "channel_id", INTEGER, "reaction_emoji", TEXT))
+            new TableModel("events", Map.of("timestamp", INTEGER, "event_type", TEXT, "user_id", INTEGER, "channel_id", INTEGER, "reaction_emoji", TEXT, "guild_id", INTEGER))
     );
 
 
